d03d7b59b0752973c02046f79258480a6b0a9a3b
root <root@localhost.localdomain>
2018-04-18 17:47:20 +0300

    initial status

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import { Goal } from './goal'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+
+    // goals = [
+    //   new Goal(1,'Watch Finding Nemo'),
+    //   new Goal(2,'Buy cookies'),
+    //   new Goal(3, 'Get new phone Case'),
+    //   new Goal(4, 'Get Dog food'),
+    //   new Goal(5, 'Solve math homework'),
+    //   new Goal(6, 'Plot my world domination plan'),
+    // ]
+  }    
